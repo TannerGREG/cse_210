6f1f7e19fe3cba14d48a845459bf617f7ad2a1ad
TannerGREG <bro24118@byui.edu>
2026-01-16 00:17:12 -0700

    fixed the prepare 4 program

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int number = 1;
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            string numberInput = Console.ReadLine();
+            number = int.Parse(numberInput);
+            numbers.Add(number);
+        }
+        numbers.Remove(0);
+        int sum = 0;
+        int large = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+            if (num > large)
+                large = num;
+        }
+        float average = (float)sum / numbers.Count;
+
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is is: {large}");
+
+        
     }
 }
